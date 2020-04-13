78013620e26f14bf0d72a5541d7ed9db984b6dbe
Oversv <albertomdv@gmail.com>
2020-04-13 11:57:00 +0100

    Updates

---
+++ precache-manifest.a5952c2773111b6931fe7127b09ff100.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48db2a9b7ffc1bfe2acb5806b035bbcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96638d3fa4fd950427dc",
+    "url": "/static/css/main.ec465577.chunk.css"
+  },
+  {
+    "revision": "1c92aceb26b30830938a",
+    "url": "/static/js/2.8b4f7954.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b4f7954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96638d3fa4fd950427dc",
+    "url": "/static/js/main.186a8112.chunk.js"
+  },
+  {
+    "revision": "d4fa88bbdd6e2d736214",
+    "url": "/static/js/runtime-main.74039080.js"
+  },
+  {
+    "revision": "f3e1419df2a1ac00aadeb8d1b62fe4c7",
+    "url": "/static/media/bell.f3e1419d.mp3"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/static/media/digital-7.1e670d88.ttf"
+  }
+]);
